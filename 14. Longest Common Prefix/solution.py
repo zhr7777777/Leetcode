31dd47eb2abdf781d3744c983a4e535d12e11c81
zhr77777777 <simonzhang2016@gmail.com>
2019-01-14 18:49:51 +0800

    modify the comment of 14

--- 14. Longest Common Prefix/solution.py
+++ 14. Longest Common Prefix/solution.py
@@ -7,11 +7,11 @@ class Solution:
         length = len(strs)
         if(length == 0):
             return ''
-        common = strs[0]    # init common prefix as the first str
+        common = strs[0]    # init the first str of strs as common prefix
         for i in range(1, length):  # compare the second and continue one by one, slice the common
             temp = ''
             for j in range(len(strs[i])):
-                if j >= len(common):
+                if j >= len(common):    # coz common[len(common) - 1] is the last
                     break
                 if strs[i][j] == common[j]:
                     temp += common[j]
