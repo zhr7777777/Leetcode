e1de8a654e41991227d765e33f0e64fa46eaf721
zhr7777777 <simonzhang2016@gmail.com>
2019-08-25 22:27:21 +0800

    Finish 101. Symmetric Tree by go

---
+++ 101. Symmetric Tree/solution.go
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSymmetric(root *TreeNode) bool {
+    if root == nil {
+        return true
+    }
+    return isSymmetricSameTree(root.Left, root.Right) 
+}
+
+func isSymmetricSameTree(p *TreeNode, q *TreeNode) bool {
+    if p == nil && q == nil {   // important
+        return true
+    }
+    if p == nil && q != nil {   // important
+        return false
+    }
+    if p != nil && q == nil {   // important
+        return false
+    }
+    return p.Val == q.Val && isSameTree(p.Left, q.Right) && isSameTree(p.Right, q.Left)
+}
