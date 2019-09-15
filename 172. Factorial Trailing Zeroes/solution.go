c666a0d4335f9ec30e640b7c354f926301360d10
zhr7777777 <simonzhang2016@gmail.com>
2019-09-15 16:44:59 +0800

    Finish 172. Factorial Trailing Zeroes by go

---
+++ 172. Factorial Trailing Zeroes/solution.go
@@ -0,0 +1,30 @@
+func trailingZeroes(n int) int {
+    pow := log5(n)
+    result := 0
+    for i:=0; i<pow; i++ {
+        result += int(n / pow5(i+1))
+    }
+    return result
+}
+
+func log5(n int) int {
+    count := 0
+    cur := 5
+    for {
+        if cur > n {
+            break
+        } else {
+            count++
+        }
+        cur *= 5
+    }
+    return count
+}
+
+func pow5(n int) int {
+    result := 1
+    for i:=0; i<n; i++ {
+        result *= 5
+    }
+    return result
+}
