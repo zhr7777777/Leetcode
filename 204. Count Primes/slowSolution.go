38d5132b3d5ca9558d2660bee2a0774558dd628f
zhr7777777 <simonzhang2016@gmail.com>
2019-09-27 23:18:52 +0800

    Finish 204. Count Primes by go

--- 204. Count Primes/slowSolution.go
+++ 204. Count Primes/slowSolution.go
@@ -33,3 +33,26 @@ func isPrime(n int, primes []int) bool {
     // }
     return true
 }
+
+func countPrimes(n int) int {       // fast solution
+    isPrime := make([]bool, n)
+    // isPrime := [n]bool{}     //  non-constant array bound n
+    for i:=2; i<n; i++ {
+        isPrime[i] = true
+    }
+    for i:=2; i*i<n; i++ {
+        if !isPrime[i] {
+            continue
+        }
+        for j:=i*i; j<n; j+=i {
+            isPrime[j] = false
+        }
+    }
+    count :=0
+    for i:=2; i<n; i++ {
+        if isPrime[i] {
+            count++
+        }
+    }
+    return count
+}
