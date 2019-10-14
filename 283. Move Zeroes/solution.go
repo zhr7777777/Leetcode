ee640c46438990bf1126e8919f5617e29935496a
zhr7777777 <simonzhang2016@gmail.com>
2019-10-15 00:05:55 +0800

    Finish 283. Move Zeroes by go

---
+++ 283. Move Zeroes/solution.go
@@ -0,0 +1,13 @@
+func moveZeroes(nums []int)  {
+    count := 0
+    for i:=0; i<len(nums); i++ {
+        if nums[i] == 0 {
+            count++
+        } else {
+            if count != 0 { // important
+                nums[i-count] = nums[i]
+                nums[i] = 0
+            }
+        }
+    }
+}
