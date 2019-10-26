e6cdab73b127ea1ea16d47194195e9892cb7a1de
zhr7777777 <simonzhang2016@gmail.com>
2019-10-26 22:16:07 +0800

    Finish 350. Intersection of Two Arrays II by go

---
+++ 350. Intersection of Two Arrays II/solution.go
@@ -0,0 +1,20 @@
+func intersect(nums1 []int, nums2 []int) []int {
+    mapping := map[int]int{}
+    result := []int{}
+    for i:=0; i<len(nums1); i++ {
+        _, has := mapping[nums1[i]]
+        if !has {
+            mapping[nums1[i]] = 1
+        } else {
+            mapping[nums1[i]] = mapping[nums1[i]] + 1
+        }
+    }
+    for i:=0; i<len(nums2); i++ {
+        sign, has := mapping[nums2[i]]
+        if has && sign != 0 {
+            mapping[nums2[i]] = mapping[nums2[i]] - 1
+            result = append(result, nums2[i])
+        }
+    }
+    return result
+}
