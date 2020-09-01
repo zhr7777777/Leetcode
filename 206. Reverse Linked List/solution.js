d61004d7ab8d37c6716f1b0834ad9650f7d9f31a
zhr7777777 <simonzhang2016@gmail.com>
2020-09-01 22:52:15 +0800

    Update 206. Reverse Linked List

--- 206. Reverse Linked List/solution.js
+++ 206. Reverse Linked List/solution.js
@@ -13,30 +13,28 @@
 // Input: 1->2->3->4->5->NULL
 // Output: 5->4->3->2->1->NULL
 
-var reverseList = function(head) {
-    if(!head) return head
-    let headNode = new ListNode()
-    headNode.next = null
-    let pre = headNode
-    let cur = head
-    while(cur) {
-        let next = cur.next
-        cur.next = pre
-        pre = cur
-        cur = next
-    }
-    head.next = null
-    return pre
-};
+const reverseList = node => {
+  let head = new ListNode()
+  head.next = node
+  let cur = node
+  let pre = head
+  while(cur) {
+    let temp = cur.next
+    cur.next = pre
+    pre = cur
+    cur = temp
+  }
+  // head = null // 这里把head置位null无效，因为只是把head指向null，node.next还是指向原来head指向的head node
+  node.next = null
+  return pre
+}
 
-// recursively
-var reverseList = function(head) {
-    if(!head) { // 考虑空链表的情况
-        return null
-    }
-    if(!head.next) return head
-    let p = reverseList(head.next)  // 递归到最后一个节点，比如上面的5
-    head.next.next = head   // 对于非最后节点的操作
-    head.next = null
-    return p
-};
+// recursive
+const reverseList = node => {
+    if(node === null) return null // 无node情况
+    if(node.next === null) return node  // 一个node情况
+    const lastNode = reverseList(node.next)
+    node.next.next = node // 两个node情况
+    node.next = null
+    return lastNode
+}
